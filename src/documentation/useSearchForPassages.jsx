196d396e78308c0c023e7c51fae14a3723eef552
Christopher Klapp <christopher@klapp.name>
2021-12-03 14:52:11 -0500

    useSearchForPassages instead of Blocks

--- src/documentation/useSearchForPassages.jsx
+++ src/documentation/useSearchForPassages.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-export default function useSearchForBlocks(props) {
+export default function useSearchForPassages(props) {
   return (<></>);
 };
 
-useSearchForBlocks.propTypes = {
+useSearchForPassages.propTypes = {
   /** Proskomma instance to query */
   proskomma: PropTypes.object,
   /** Change Index to synchronize Proskomma updates/imports */
@@ -17,11 +17,14 @@ useSearchForBlocks.propTypes = {
   tokens: PropTypes.bool,
   /** Text to search for */
   text: PropTypes.string,
+  /** Search in blocks not verses */
+  blocks: PropTypes.bool,
 };
 
-useSearchForBlocks.defaultProps = {
+useSearchForPassages.defaultProps = {
   proskomma: undefined,
   stateId: 0,
   text: '',
   tokens: false,
+  blocks: false,
 };
