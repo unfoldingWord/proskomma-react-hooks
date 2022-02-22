5187d3fcfe2078c27c033b76c07872c3dc0a3ab8
klappy <christopher@klapp.name>
2022-02-22 15:18:11 -0500

    github ribbon

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const upperFirst = require('lodash/upperFirst');
+const camelCase = require('lodash/camelCase');
+
+const {
+  name, version, repository,
+} = require('./package.json');
+
 module.exports = {
   pagePerSection: true,
   usageMode: 'expand',
@@ -6,13 +13,18 @@ module.exports = {
   components: 'src/documentation/**/*.{js,jsx,ts,tsx}',
   moduleAliases: { 'proskomma-react-hooks': path.resolve(__dirname, 'src') },
   getComponentPathLine: componentPath => {
-    const name = path.basename(componentPath, '.js')
-    return `import { ${name.split('.')[0]} } from 'proskomma-react-hooks';`
+    const name = path.basename(componentPath, '.js');
+    return `import { ${name.split('.')[0]} } from 'proskomma-react-hooks';`;
   },
   handlers: componentPath => (
     require('react-docgen').defaultHandlers.concat(
       require('react-docgen-external-proptypes-handler')(componentPath),
-      require('react-docgen-displayname-handler').createDisplayNameHandler(componentPath)
+      require('react-docgen-displayname-handler').createDisplayNameHandler(componentPath),
     )
   ),
+  title: `${upperFirst(camelCase(name))} v${version}`,
+  ribbon: {
+    url: repository.url,
+    text: 'View on GitHub',
+  },
 };
